eb18c7babfb4f63aa4225a9f7280deca43572771
jameslmartin <james.martin@guildeducation.com>
2024-11-13 17:08:08 -0500

    Simplify

--- src/acrostic/generator.py
+++ src/acrostic/generator.py
@@ -3,17 +3,15 @@ from wonderwords import RandomWord
 
 class AcrosticGenerator:
     __generator = RandomWord()
-    
-    @classmethod
-    def get_random_word(cls, starting_letter):
-        return cls.__generator.word(
-            starts_with=starting_letter,
-            include_parts_of_speech=["adjectives"]
-        )
 
     @classmethod
     def generate(cls, key: str) -> List[str]:
         acrostic = []
         for char in list(key):
-            acrostic.append(AcrosticGenerator.get_random_word(char))
+            acrostic.append(
+                cls.__generator.word(
+                    starts_with=char,
+                    include_parts_of_speech=["adjectives"]
+                )
+            )
         return acrostic
